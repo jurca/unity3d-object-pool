bb23e021bcb598668fdddf7740334a548a24d3ea
Martin Jurča <mjurca@centrum.cz>
2017-09-04 21:34:58 +0200

    Added the implementation

---
+++ ObjectPool.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectPool {
+	private static uint lastInstanceId = 0;
+
+	public int minCapacity;
+	public int maxCapacity;
+	public GameObject prefab;
+	public bool returnNullOnUnderflow;
+	public Func<GameObject, GameObject> initializer = (prefab) => GameObject.Instantiate (prefab);
+	public string label = "<Unlabeled object pool #" + ++ObjectPool.lastInstanceId + ">";
+
+	private GameObject[] pool;
+	private bool ready = false;
+	private int queueStart = 0;
+	private int queueEnd = 0;
+	private int objectsInPool = 0;
+
+	public GameObject GetObject() {
+		if (!ready) {
+			Init ();
+		}
+
+		if (objectsInPool == 0) {
+			if (pool.LongLength < maxCapacity) {
+				Grow ();
+			} else if (returnNullOnUnderflow) {
+				return null;
+			} else {
+				throw new OutOfMemoryException(
+					"The maximum capacity (" + maxCapacity + ") of the object pool '" + label + "' has been exceeded"
+				);
+			}
+		}
+
+		GameObject objectInstance = pool[queueStart];
+		objectInstance.SetActive (true);
+		queueStart = (queueStart + 1) % pool.Length;
+		objectsInPool--;
+		return objectInstance;
+	}
+
+	public void ReturnObject(GameObject usedObject) {
+		if (!ready) {
+			Init ();
+		}
+
+		usedObject.SetActive (false);
+		pool [queueEnd] = usedObject;
+		queueEnd = (queueEnd + 1) % pool.Length;
+		objectsInPool++;
+	}
+
+	public void Init() {
+		if (ready) {
+			return;
+		}
+		ready = true;
+
+		if (prefab == null) {
+			throw new ArgumentNullException ("The prefab game object cannot be null");
+		}
+		if (initializer == null) {
+			throw new ArgumentNullException ("The game object instance initializer function cannot be null");
+		}
+		if (minCapacity <= 0) {
+			throw new ArgumentOutOfRangeException ("The minimum capacity must be a positive integer");
+		}
+		if (maxCapacity < minCapacity) {
+			throw new ArgumentOutOfRangeException (
+				"The maximum capacity (" + maxCapacity + ") cannot be lower than the minimum capacity (" + minCapacity + ")"
+			);
+		}
+
+		pool = new GameObject[minCapacity];
+		for (int i = 0; i < minCapacity; i++) {
+			AddObjectInstance ();
+		}
+	}
+
+	private void Grow() {
+		Debug.LogWarning (
+			"The currenct capacity (" + pool.LongLength + ") of the pool '" + label +
+			"' has been exceeded, increasing capacity"
+		);
+
+		int newCapacity;
+		if (pool.Length > maxCapacity / 2) {
+			newCapacity = maxCapacity;
+		} else {
+			newCapacity = pool.Length * 2;
+		}
+		int capacityIncrease = newCapacity - pool.Length;
+		pool = new GameObject[newCapacity];
+
+		for (int i = 0; i < capacityIncrease; i++) {
+			AddObjectInstance ();
+		}
+	}
+
+	private void AddObjectInstance() {
+		GameObject objectInstance = initializer (prefab);
+		PooledObjectController controller = objectInstance.GetComponent<PooledObjectController> ();
+		controller.SetParentPool (this);
+		ReturnObject (objectInstance);
+	}
+}
---
+++ PooledObjectController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PooledObjectController : MonoBehaviour {
+	private ObjectPool parentPool;
+
+	public void SetParentPool(ObjectPool newParentPool) {
+		if (parentPool != null) {
+			throw new InvalidOperationException ("Cannot reconfigure the parent pool reference once it has been set");
+		}
+
+		parentPool = newParentPool;
+	}
+
+	public void ReturnToPool() {
+		parentPool.ReturnObject (this.gameObject);
+	}
+}
